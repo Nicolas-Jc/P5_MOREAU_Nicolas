e1d3dae46c4949cd3492254726a063d04026d212
Nicolas Moreau <nicolas.jc.moreau@outlook.com>
2022-06-20 16:33:13 +0200

    Finalisation Tests Unitaires Package "Service"

--- src/test/java/com/openclassrooms/safetynet/service/PersonServiceTest.java
+++ src/test/java/com/openclassrooms/safetynet/service/PersonServiceTest.java
@@ -3,9 +3,7 @@ package com.openclassrooms.safetynet.service;
 import com.openclassrooms.safetynet.dao.FireStationDAO;
 import com.openclassrooms.safetynet.dao.MedicalRecordsDAO;
 import com.openclassrooms.safetynet.dao.PersonDAO;
-import com.openclassrooms.safetynet.dto.CommunityEmailDTO;
-import com.openclassrooms.safetynet.dto.InfoPersonDetailedDTO;
-import com.openclassrooms.safetynet.dto.PhoneAlertDTO;
+import com.openclassrooms.safetynet.dto.*;
 import com.openclassrooms.safetynet.model.MedicalRecord;
 import com.openclassrooms.safetynet.model.Person;
 import org.junit.jupiter.api.Test;
@@ -117,10 +115,88 @@ public class PersonServiceTest {
                 , allergies);
         Mockito.when(medicalRecordService.findMedicalRecord(any(String.class), any(String.class)))
                 .thenReturn(medicalRecord);
+        Mockito.when(calculateFonction.calculateAge(any(Date.class))).thenReturn(25);
         // WHEN
         List<InfoPersonDetailedDTO> listInfoPerson = personService.getPersonByFirstLastName("firstName", "lastName");
         // THEN
         assertThat(listInfoPerson.size()).isEqualTo(3);
+        assertThat(listInfoPerson.get(0).getMedications()).isEqualTo(medicalRecord.getMedications());
+        assertThat(listInfoPerson.get(0).getAllergies()).isEqualTo(medicalRecord.getAllergies());
+    }
+
+    @Test
+    void getChildAlertByAddressTest() throws Exception {
+        // GIVEN
+        List<Person> listPersons = new ArrayList<>();
+        Person person1 = new Person("firstName1", "lastName1", "adress1", "City1", "", "", "email1@test.fr");
+        Person person2 = new Person("firstName2", "lastName2", "adress2", "City2", "", "", "email2@test.fr");
+        Person person3 = new Person("firstName3", "lastName3", "adress3", "City2", "", "", "email3@test.fr");
+        listPersons.add(person1);
+        listPersons.add(person2);
+        listPersons.add(person3);
+        Mockito.when(personDAO.findPersonByAdress(any(String.class))).thenReturn(listPersons);
+
+        String Date = "01/01/2000";
+        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        Date birthDate = dateFormat.parse(Date);
+        List<String> medications = new ArrayList<String>();
+        medications.add("medication1");
+        List<String> allergies = new ArrayList<String>();
+        allergies.add("allergie1");
+        MedicalRecord medicalRecord = new MedicalRecord("firstName1"
+                , "lastName1"
+                , birthDate
+                , medications
+                , allergies);
+        Mockito.when(medicalRecordService.findMedicalRecord(any(String.class), any(String.class)))
+                .thenReturn(medicalRecord);
+        Mockito.when(calculateFonction.calculateAge(any(Date.class))).thenReturn(17);
+        // WHEN
+        List<ChildAlertDTO> listChildAlert = personService.getChildAlertByAddress("addressToFind");
+        // THEN
+        assertThat(listChildAlert.size()).isEqualTo(3);
+        assertThat(listChildAlert.get(0).getFirstName()).isEqualTo(person1.getFirstName());
+        assertThat(listChildAlert.get(1).getFirstName()).isEqualTo(person2.getFirstName());
+        assertThat(listChildAlert.get(2).getFirstName()).isEqualTo(person3.getFirstName());
+
+    }
+
+    @Test
+    void getPersonByAddressTest() throws Exception {
+        // GIVEN
+        List<Person> listPersons = new ArrayList<>();
+        Person person1 = new Person("firstName1", "lastName1", "adress1", "City1", "", "", "email1@test.fr");
+        Person person2 = new Person("firstName2", "lastName2", "adress2", "City2", "", "", "email2@test.fr");
+        Person person3 = new Person("firstName3", "lastName3", "adress3", "City2", "", "", "email3@test.fr");
+        listPersons.add(person1);
+        listPersons.add(person2);
+        listPersons.add(person3);
+        Mockito.when(personDAO.findPersonByAdress(any(String.class))).thenReturn(listPersons);
+
+        String Date = "01/01/2000";
+        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        Date birthDate = dateFormat.parse(Date);
+        List<String> medications = new ArrayList<String>();
+        medications.add("medication1");
+        List<String> allergies = new ArrayList<String>();
+        allergies.add("allergie1");
+        MedicalRecord medicalRecord = new MedicalRecord("firstName1"
+                , "lastName1"
+                , birthDate
+                , medications
+                , allergies);
+        Mockito.when(medicalRecordService.findMedicalRecord(any(String.class), any(String.class)))
+                .thenReturn(medicalRecord);
+        Mockito.when(calculateFonction.calculateAge(any(Date.class))).thenReturn(45);
+        Mockito.when(fireStationDAO.getStationByAddress(any(String.class))).thenReturn("3");
+        // WHEN
+        List<FireDTO> listFireDTO = personService.getPersonByAddress("addressToFind");
+        // THEN
+        assertThat(listFireDTO.size()).isEqualTo(3);
+        assertThat(listFireDTO.get(0).getFirstName()).isEqualTo(person1.getFirstName());
+        assertThat(listFireDTO.get(1).getFirstName()).isEqualTo(person2.getFirstName());
+        assertThat(listFireDTO.get(2).getFirstName()).isEqualTo(person3.getFirstName());
+
     }
 
 }
